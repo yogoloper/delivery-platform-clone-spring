0d9c990eb52982d673a4c2d7f2c3681a1edc3536
yogoloper <jguddnjs@gmail.com>
2025-10-06 16:26:48 +0900

    api 공통 spec 작성

--- api/src/main/java/org/delivery/api/account/AccountApiController.java
+++ api/src/main/java/org/delivery/api/account/AccountApiController.java
@@ -2,6 +2,7 @@ package org.delivery.api.account;
 
 import lombok.RequiredArgsConstructor;
 import org.delivery.api.account.model.AccountMeResponse;
+import org.delivery.api.common.api.Api;
 import org.delivery.db.account.AccountEntity;
 import org.delivery.db.account.AccountRepository;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,11 +19,13 @@ public class AccountApiController {
     private final AccountRepository accountRepository;
 
     @GetMapping("/me")
-    public AccountMeResponse me() {
-        return AccountMeResponse.builder()
+    public Api<AccountMeResponse> me() {
+        var response = AccountMeResponse.builder()
                 .name("홍길동")
                 .email("homg@gmail.com")
                 .registeredAt(LocalDateTime.now())
                 .build();
+
+        return Api.OK(response);
     }
 }
