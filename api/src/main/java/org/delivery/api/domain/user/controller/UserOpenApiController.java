8653cba21666d8c761325675eb4beeebec322390
yogoloper <jguddnjs@gmail.com>
2025-10-07 00:55:45 +0900

    사용자 로직 작성 - 회원 가입

---
+++ api/src/main/java/org/delivery/api/domain/user/controller/UserOpenApiController.java
@@ -0,0 +1,27 @@
+package org.delivery.api.domain.user.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.delivery.api.common.api.Api;
+import org.delivery.api.domain.user.business.UserBusiness;
+import org.delivery.api.domain.user.controller.model.UserRegisterRequest;
+import org.delivery.api.domain.user.controller.model.UserResponse;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/open-api/user")
+public class UserOpenApiController {
+
+    private final UserBusiness userBusiness;
+
+    // 사용자 가입 요청
+    @PostMapping("/register")
+    public Api<UserResponse> register(
+            @Valid
+            @RequestBody Api<UserRegisterRequest> request
+    ) {
+        var response = userBusiness.register(request.getBody());
+        return Api.OK(response);
+    }
+}
