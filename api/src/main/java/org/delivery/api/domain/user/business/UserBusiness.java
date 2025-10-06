347d73378407ecad03410fa86a1a2441f5f45def
yogoloper <jguddnjs@gmail.com>
2025-10-07 01:10:02 +0900

    사용자 로직 작성 - 로그인

--- api/src/main/java/org/delivery/api/domain/user/business/UserBusiness.java
+++ api/src/main/java/org/delivery/api/domain/user/business/UserBusiness.java
@@ -4,10 +4,12 @@ import lombok.RequiredArgsConstructor;
 import org.delivery.api.common.annotation.Business;
 import org.delivery.api.common.error.ErrorCode;
 import org.delivery.api.common.exception.ApiException;
+import org.delivery.api.domain.user.controller.model.UserLoginRequest;
 import org.delivery.api.domain.user.controller.model.UserRegisterRequest;
 import org.delivery.api.domain.user.controller.model.UserResponse;
 import org.delivery.api.domain.user.converter.UserConverter;
 import org.delivery.api.domain.user.service.UserService;
+import org.delivery.db.user.UserEntity;
 
 import java.util.Optional;
 
@@ -40,4 +42,21 @@ public class UserBusiness {
 //                .map(userConverter::toResponse)
 //                .orElseThrow(() -> new ApiException(ErrorCode.NULL_POINT, "UserRegisterRequest null"));
     }
+
+    /**
+     * 1. email, password 를 가지고 사용자 체크
+     * 2. user entity 로그인 확인
+     * 3. token 생성
+     * 4. token response
+     */
+    public UserResponse login(UserLoginRequest request) {
+        var userEntity = userService.login(request.getEmail(), request.getPassword());
+        // 사용자 없으면 throw
+
+
+        // TODO 토큰 생성
+
+        return userConverter.toResponse(userEntity);
+    }
+
 }
--- api/src/main/java/org/delivery/api/domain/user/controller/UserOpenApiController.java
+++ api/src/main/java/org/delivery/api/domain/user/controller/UserOpenApiController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.delivery.api.common.api.Api;
 import org.delivery.api.domain.user.business.UserBusiness;
+import org.delivery.api.domain.user.controller.model.UserLoginRequest;
 import org.delivery.api.domain.user.controller.model.UserRegisterRequest;
 import org.delivery.api.domain.user.controller.model.UserResponse;
 import org.springframework.web.bind.annotation.*;
@@ -24,4 +25,15 @@ public class UserOpenApiController {
         var response = userBusiness.register(request.getBody());
         return Api.OK(response);
     }
+
+    // 로그인
+    @PostMapping("/login")
+    public Api<UserResponse> login(
+        @Valid
+        @RequestBody Api<UserLoginRequest> request
+    ) {
+        var response = userBusiness.login(request.getBody());
+        return Api.OK(response);
+    }
+
 }
