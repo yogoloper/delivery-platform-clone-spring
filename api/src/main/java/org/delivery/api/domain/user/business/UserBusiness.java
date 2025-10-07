d1a035d102aae4f192dd10fb0fca022c7f9873a4
yogoloper <jguddnjs@gmail.com>
2025-10-07 16:07:36 +0900

    사용자 로직 작성 - 로그인 토큰 발행

--- api/src/main/java/org/delivery/api/domain/user/business/UserBusiness.java
+++ api/src/main/java/org/delivery/api/domain/user/business/UserBusiness.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.delivery.api.common.annotation.Business;
 import org.delivery.api.common.error.ErrorCode;
 import org.delivery.api.common.exception.ApiException;
+import org.delivery.api.domain.token.controller.business.TokenBusiness;
+import org.delivery.api.domain.token.controller.model.TokenResponse;
 import org.delivery.api.domain.user.controller.model.UserLoginRequest;
 import org.delivery.api.domain.user.controller.model.UserRegisterRequest;
 import org.delivery.api.domain.user.controller.model.UserResponse;
@@ -20,6 +22,8 @@ public class UserBusiness {
     private final UserService userService;
 
     private final UserConverter userConverter;
+
+    private final TokenBusiness tokenBusiness;
 
     /**
      * 사용자에 대한 가입처리 로직
@@ -49,14 +53,10 @@ public class UserBusiness {
      * 3. token 생성
      * 4. token response
      */
-    public UserResponse login(UserLoginRequest request) {
+    public TokenResponse login(UserLoginRequest request) {
         var userEntity = userService.login(request.getEmail(), request.getPassword());
-        // 사용자 없으면 throw
-
-
-        // TODO 토큰 생성
-
-        return userConverter.toResponse(userEntity);
+        var tokenResponse = tokenBusiness.issueToken(userEntity);
+        return tokenResponse;
     }
 
 }
