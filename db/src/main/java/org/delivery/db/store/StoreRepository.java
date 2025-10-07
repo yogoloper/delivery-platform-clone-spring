c4dbfe3e97ca4386344766f53593cfcd614fc266
yogoloper <jguddnjs@gmail.com>
2025-10-08 02:09:57 +0900

    스토어 디비 수정

--- db/src/main/java/org/delivery/db/store/StoreRepository.java
+++ db/src/main/java/org/delivery/db/store/StoreRepository.java
@@ -4,6 +4,7 @@ import org.delivery.db.store.enums.StoreCategory;
 import org.delivery.db.store.enums.StoreStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StoreRepository extends JpaRepository<StoreEntity, Long> {
@@ -12,8 +13,8 @@ public interface StoreRepository extends JpaRepository<StoreEntity, Long> {
     Optional<StoreEntity> findFirstByIdAndStatusOrderByIdDesc(Long id, StoreStatus status);
 
     // 유요한 스토어 리스트
-    Optional<StoreEntity> findByStatusOrderByIdDesc(StoreStatus status);
+    List<StoreEntity> findAllByStatusOrderByIdDesc(StoreStatus status);
 
     // 유요한 특정 카테고리의 스토어 리스트
-    Optional<StoreEntity> findByStatusAndCategoryOrderByStarDesc(StoreStatus status, StoreCategory category);
+    List<StoreEntity> findAllByStatusAndCategoryOrderByStarDesc(StoreStatus status, StoreCategory category);
 }
