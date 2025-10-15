10e1ffa952c0aa6ef06cb1d5b6a8bd6b5b3cad6b
yogoloper <jguddnjs@gmail.com>
2025-10-15 15:40:28 +0900

    코틀린 전환 설정 - Main 파일 패키지 경로 변경

--- api/src/main/kotlin/org/delivery/api/domain/TempApiController.kt
+++ api/src/main/kotlin/org/delivery/api/domain/TempApiController.kt
@@ -1,4 +1,4 @@
-package org.delivery.api.domain.temp
+package org.delivery.api.domain
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
