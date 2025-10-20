8610ec6a79247bd0e6835d68b94a3a28b2e3cd06
yogoloper <jguddnjs@gmail.com>
2025-10-20 23:30:19 +0900

    게이트웨이 설정 - apigw <-> account 모듈간 연동

--- service/account/src/main/kotlin/org/delivery/account/config/objectmapper/ObjectMapperConfig.kt
+++ service/account/src/main/kotlin/org/delivery/account/config/objectmapper/ObjectMapperConfig.kt
@@ -1,4 +1,4 @@
-package org.delivery.api.config.objectmapper
+package org.delivery.account.config.objectmapper
 
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
--- service/apigw/build.gradle.kts
+++ service/apigw/build.gradle.kts
@@ -27,6 +27,8 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation(project(":common"))
 }
 
 dependencyManagement {
