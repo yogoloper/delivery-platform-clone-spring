799b1af1239839f60310c8ff0a56ab6b354ad2ac
yogoloper <jguddnjs@gmail.com>
2025-10-20 15:48:02 +0900

    게이트웨이 멀티 모듈로 생성

---
+++ service/apigw/build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.spring")
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+}
+
+group = "com.delivery"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+extra["springCloudVersion"] = "2025.0.0"
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.cloud:spring-cloud-starter-gateway-server-webmvc")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ service/apigw/src/main/kotlin/com/delivery/apigw/ApiGWApplication.kt
@@ -0,0 +1,13 @@
+package com.delivery.apigw
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiGWApplication {
+
+}
+
+fun main(args: Array<String>) {
+    runApplication<ApiGWApplication>(*args)
+}
