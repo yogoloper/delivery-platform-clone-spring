9c734a6764c0a58c39eab5f2b9be56899f96459d
yogoloper <jguddnjs@gmail.com>
2025-10-22 11:24:38 +0900

    모니터링 - 로그백 설정

--- service/api/src/main/java/org/delivery/api/domain/user/controller/UserOpenApiController.java
+++ service/api/src/main/java/org/delivery/api/domain/user/controller/UserOpenApiController.java
@@ -2,6 +2,7 @@ package org.delivery.api.domain.user.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.delivery.common.api.Api;
 import org.delivery.api.domain.token.controller.model.TokenResponse;
 import org.delivery.api.domain.user.business.UserBusiness;
@@ -9,7 +10,10 @@ import org.delivery.api.domain.user.controller.model.UserLoginRequest;
 import org.delivery.api.domain.user.controller.model.UserRegisterRequest;
 import org.delivery.api.domain.user.controller.model.UserResponse;
 import org.springframework.web.bind.annotation.*;
+import org.delivery.common.exception.ApiException;
+import org.delivery.common.error.ErrorCode;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/open-api/user")
@@ -33,6 +37,7 @@ public class UserOpenApiController {
         @Valid
         @RequestBody Api<UserLoginRequest> request
     ) {
+//        if (true) throw new ApiException(ErrorCode.SERVER_ERROR);
         var response = userBusiness.login(request.getBody());
         return Api.OK(response);
     }
